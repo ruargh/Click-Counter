9ee6278439b48ccff9bcbe49ff0343d96d42abdb
Ruargh Steyn <ruargh@gmail.com>
2021-05-22 17:21:48 +0200

    lesson 1 - 10

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Label
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        self.label = label
+        
+        // Button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
+    }
+    
+    @objc func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
     }
 
 
